02f3021a2106db0073ade2b70c7f0715dfd8fd0b
Pieter Wigboldus <pieter.wigboldus@way2web.nl>
2018-05-11 16:44:18 +0200

    EASCON-161: fix the lint script

--- lintHook.sh
+++ lintHook.sh
@@ -14,35 +14,32 @@ fi
 
 echo "====== Running linters ======="
 
-for gulpfile in admin front
-do
-    echo "=== $gulpfile start ==="
-    cd $path/resources/assets/$gulpfile
+echo "=== $gulpfile start ==="
+cd $path/$gulpfile
 
-    if [ -f package.json ]; then
-        find . -maxdepth 1 -name package.json | grep package > /dev/null 2>&1
-        if [ $? == 0 ]; then
-            echo "= yarn ="
-            yarn install
-            if [ $? != 0 ]; then
-                exit 1
-            fi
+if [ -f package.json ]; then
+    find . -maxdepth 1 -name package.json | grep package > /dev/null 2>&1
+    if [ $? == 0 ]; then
+        echo "= yarn ="
+        yarn install
+        if [ $? != 0 ]; then
+            exit 1
         fi
-
-        if [ -f node_modules/gulp/bin/gulp.js ]; then
-            echo "= gulp ="
-            npm run lint
-            if [ $? != 0 ]; then
-                exit 1
-            fi
-        else
-            echo "No gulp installed"
-        fi
-    else
-        echo "Package.json doesn't exist"
     fi
 
-    echo "=== $gulpfile end ==="
-done
+    if [ -f node_modules/gulp/bin/gulp.js ]; then
+        echo "= gulp ="
+        npm run lint
+        if [ $? != 0 ]; then
+            exit 1
+        fi
+    else
+        echo "No gulp installed"
+    fi
+else
+    echo "Package.json doesn't exist"
+fi
+
+echo "=== $gulpfile end ==="
 
 echo "================================="
