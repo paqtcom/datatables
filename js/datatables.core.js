a46b9a29c158c904e0098921af923f98b22f8762
Pieter Wigboldus <pieter.wigboldus@way2web.nl>
2018-05-11 16:26:21 +0200

    EASCON-161: fix the version in the script

--- js/datatables.core.js
+++ js/datatables.core.js
@@ -11,7 +11,7 @@
 window.DataTable = function($table, userOptions, eventOptions, translations) {
     'use strict';
 
-    var version = '0.2.6';
+    var version = '0.3.0';
 
     var elements = {
         columnRowSelector:  '.js-table-columns',
